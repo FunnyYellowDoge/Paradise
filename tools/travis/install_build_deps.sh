fac285048524a364740f1667a1479d6d21582ae0
AffectedArc07 <josephcase55@hotmail.com>
2020-04-12 14:23:37 +0100

    Test of the travis cleanup

--- tools/travis/install_build_deps.sh
+++ tools/travis/install_build_deps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-source _build_dependencies.sh
+source dependencies.sh
 
 source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
 
-pip install --user PyYaml -q
-pip install --user beautifulsoup4 -q
+pip3 install --user PyYaml
+pip3 install --user beautifulsoup4
 
 phpenv global $PHP_VERSION
