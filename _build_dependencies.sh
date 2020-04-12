5fcfd64037efe5288250228b9296eb6949b6353a
AffectedArc07 <josephcase55@hotmail.com>
2020-04-12 14:23:37 +0100

    Test of the travis cleanup

--- _build_dependencies.sh
+++ _build_dependencies.sh
@@ -2,10 +2,6 @@
 # For dreamchecker
 export SPACEMANDMM_TAG=suite-1.2
 # For NanoUI
-export NODE_VERSION=9
+export NODEJS_VERSION=9
 # For the scripts in tools
 export PHP_VERSION=5.6
-# Byond Major
-export BYOND_MAJOR=513
-# Byond Minor
-export BYOND_MINOR=1505
--- tools/travis/build_nanoui.sh
+++ tools/travis/build_nanoui.sh
@@ -3,6 +3,5 @@ set -euo pipefail
 
 npm install -g gulp-cli
 cd nano
-source ~/.nvm/nvm.sh
 npm install --loglevel=error
 node node_modules/gulp/bin/gulp.js --require less-loader
