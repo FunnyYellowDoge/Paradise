699590cca2e4bae0b0da0fbebd8da56bee5db4e8
Foxterosa <ale.285@hotmail.com>
2020-05-02 18:17:48 -0300

    Ahora si travis?
    Por favor

--- tools/travis/install_dreamchecker.sh
+++ tools/travis/install_dreamchecker.sh
@@ -2,5 +2,5 @@
 source _build_dependencies.sh
 
 wget -O ~/dreamchecker "https://github.com/SpaceManiac/SpacemanDMM/releases/download/$SPACEMANDMM_TAG/dreamchecker"
-chmod +x ~../../dreamchecker
+chmod +x ~./dreamchecker
 ~/dreamchecker --version
